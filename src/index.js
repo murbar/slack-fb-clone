37cfd864cddfac555aa214bfb33818bb5537fc7e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-29 10:54:17 -0400

    Scaffold auth components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './components/App';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
 );
